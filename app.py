fc491f2c8f1dc9cbdb24f81b1f7b31168a078c5f
host1812 <host1812@gmail.com>
2021-01-08 16:35:13 -0800

    reset application

--- app.py
+++ app.py
@@ -8,22 +8,5 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/signup_form")
-def signup_form():
-    return render_template("signup.html")
-
-
-@app.route("/thank_you")
-def thank_you():
-    first = request.args.get("first")
-    last = request.args.get("last")
-    return render_template("thankyou.html", first=first, last=last)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
-
 if __name__ == "__main__":
     app.run(debug=True)
